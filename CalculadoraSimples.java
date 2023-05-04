f95a8d7f70f0f901640aecfa044788f8258d8269
00julia <130668482+00julia@users.noreply.github.com>
2023-05-04 08:40:56 -0300

    Update CalculadoraSimples.java

--- CalculadoraSimples.java
+++ CalculadoraSimples.java
@@ -1,3 +1,60 @@
-!do {
-    
-} while (condition);
+import java.util.Scanner;
+
+public class CalculadoraSimples{
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        //Ana Zinato
+        //declaração de variaveis 
+        int n1, n2, total, op;
+
+         //criação de instancia do objeto de entrada
+         Scanner entrada = new Scanner(System.in);
+         do{
+
+            System.out.println("\n\t\t\t --Calculadora Simples-- \n");
+
+            System.out.println("1. Soma");
+            System.out.println("2. Subtração");
+            System.out.println("3. Sair");
+
+            System.out.print("Opção: ");
+
+            op = entrada.nextInt();
+
+            if(op == 1){
+                System.out.println("\n\t\t\t -- Soma-- \n");
+                //Júlia Mogi
+                //Entrada 
+                System.out.print("informe N1:");
+                n1 = entrada.nextInt();
+                 System.out.print("informe N2:");
+                n2 = entrada.nextInt();
+
+                //Processamento
+                total = n1+n2;
+
+                //saida 
+                System.out.printf("%d + %d = %d", n1, n2, total);
+            }else if(op ==2){
+                System.out.println("\n\t\t\t -- Subtração-- \n");
+                //Laura 
+                //Entrada 
+                System.out.print("informe N1:");
+                n1 = entrada.nextInt();
+                 System.out.print("informe N2:");
+                n2 = entrada.nextInt();
+
+                //Processamento
+                total = n1-n2;
+
+                //saida 
+                System.out.printf("%d - %d = %d", n1, n2, total);
+            }else if(op == 3){
+                System.out.println("Opção" + "incorreta!");
+            }
+         }while(op!=3);
+    }
+}
